c80e600786596713cdade4b674a77a1723697e0e
azim024471-netizen <azim024471@gmail.com>
2026-06-29 12:06:37 +0600

    secure the backend and th

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ async function run() {
 
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
     const db = client.db('Taste_Trove');
     const recipeCollection = db.collection('recipes')
